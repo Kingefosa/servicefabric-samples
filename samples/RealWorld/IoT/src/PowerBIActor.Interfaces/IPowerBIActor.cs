c419b95bff369b93158469d944c9b363f6d54f3e
khenidak <khnidk@microsoft.com>
2015-10-14 16:48:42 +0000

    Power BI Actor

---
+++ samples/RealWorld/IoT/src/PowerBIActor.Interfaces/IPowerBIActor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+
+namespace PowerBIActor.Interfaces
+{
+    public interface IPowerBIActor : IActor
+    {
+        Task Post(string DeviceId, string EventHubName, string ServiceBusNS, byte[] Body);
+    }
+}
---
+++ samples/RealWorld/IoT/src/PowerBIActor/PowerBIWorkItem.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerBIActor
+{
+    public class PowerBIWorkItem
+    {
+        public string DeviceId { get; set; } = string.Empty;
+        public string EventHubName { get; set; } = string.Empty;
+        public string ServiceBusNS { get; set; } = string.Empty;
+        public byte[] Body { get; set; }
+
+        public JObject toJObject()
+        {
+            var j = JObject.Parse(Encoding.UTF8.GetString(Body));
+            j.Add("EventHubName", EventHubName);
+            j.Add("ServiceBusNS", ServiceBusNS);
+
+            return j;
+        }
+    }
+}
