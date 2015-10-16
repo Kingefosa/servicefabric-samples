cf86549af227c5be93074a058d2840e920a577a7
Matthew Snider <masnider@microsoft.com>
2015-10-16 15:19:51 -0700

    Update to 4.3.111.9494

--- samples/RealWorld/Web/ReferenceApp/Mocks/MockTransaction.cs
+++ samples/RealWorld/Web/ReferenceApp/Mocks/MockTransaction.cs
@@ -29,7 +29,7 @@ namespace Mocks
         {
         }
 
-        public Task<long> GetVisibilitySeqeuenceNumberAsync()
+        public Task<long> GetVisibilitySequenceNumberAsync()
         {
             return Task.FromResult(0L);
         }
